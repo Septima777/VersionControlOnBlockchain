c2b9b2a4a9e21c785cf7a134f8c2ec5b8d6c2bab
Septima777 <noppawankulchol@hotmail.com>
2019-07-28 00:42:49 +0900

    [Update:] new answer file and Data.sol

--- contracts/Data.sol
+++ contracts/Data.sol
@@ -5,50 +5,37 @@ pragma experimental ABIEncoderV2;
 /// @title Data
 /// @author Noppawan Kulchol
 /// @dev This contract was created to stored data
-///      both stack overflow info and code snippet on blockchain.
+///      both stack overflow information and code snippet on blockchain.
 contract Data {
     
     // event of adding a new answer to the question
-    event newAnswerAdded(string _url, string _newAnswerBody, string _newAnswerAuthor,
-    string _newAnswerEditor, uint _newAnswerCreationDate, uint _newAnswerLastModified,
-    uint _newAnswerLastActivity);
+    // event newAnswerAdded(string _url, string _newAnswerBody, string _newAnswerAuthor,
+    // string _newAnswerEditor, uint _newAnswerCreationDate, uint _newAnswerLastModified,
+    // uint _newAnswerLastActivity);
 
-    // event of adding the question's detail
-    event newDetailQuestionAdded(string _url, string _title, uint viewCount,
-    string _author, string _editor);
+    // // event of adding the question's detail
+    // event newDetailQuestionAdded(string _url, string _title, uint viewCount,
+    // string _author, string _editor);
 
-    // event of adding the question's time
-    event newTimeQuestionAdded( uint _creationDate, uint _lastActivity,uint _lastModified);
+    // // event of adding the question's time
+    // event newTimeQuestionAdded( uint _creationDate, uint _lastActivity,uint _lastModified);
 
-    // event of adding code snippet to url
-    // event newCodeSnippetAdded(string _url, string _date, string _language, string _projectName,
-    // string _companyName, string _projectPath, string _projectAuthor);
-
-    // uuid which was generated in code snippet
-    // uint public uuid = 0;
-    // array of stack overflow info
-    // StackOverflowInfo[] info;
-    // array of answer
-    // Answer[] answers;
+    // array that collect answer link
     string[] answerLink;
-    // map answer (struct) with url (string)
-    mapping (string => Answer) public getAnswers;
-
+    // map question url to question data
     mapping (string => Question) public getQuestion;
+    // map question url to answer
     mapping (string => string[]) public getAnswer;
-    // map code snippet (array of struct) with url (string)
-    // mapping (string => CodeSnippet[]) public getCodeSnippet;
-    // map stack overflow info (struct) with url (string)
-    // mapping (string => StackOverflowInfo) public getInfo;
-    //
+    // map answer url to answer
+    mapping (string => string[]) public getOneAnswer;
+    // map code snippet url to code snippet data
     mapping (string => Snippet[]) public getSnippet;
+    // map question url to question UUID
     mapping (string => uint) public getUUIDFromQuestion;
+    // map answer url to answer UUID
     mapping (string => uint[]) public getUUIDFromAnswer;
 
-    // mapping (string => address) public addressQuestioner;
-    // address private _owner;
-
-    // This is a struct of stack overflow info
+    // This is a struct of question from stack overflow information
     struct Question {
         string url;
         string data;
@@ -63,17 +50,6 @@ contract Data {
     }
 
     // This is a struct of answer of each question
-    struct Answer {
-        string url;
-        string answerBody;
-        string answerAuthor;
-        string answerEditor;
-        bool isValue;
-        uint answerCreationDate;
-        uint answerLastModified;
-        uint answerLastActivity;
-    }
-
     struct NewAnswer {
         string url;
         string data;
@@ -83,6 +59,10 @@ contract Data {
     //     _owner = msg.sender;
     // }
 
+    ///@param _url url from csv file
+    ///@param _link question url (if url is an answer link)
+    ///@param _data detail of question
+    // create question and answer
     function createNewQuestion(string memory _url,string memory _link, string memory _data) public {
         // if _link equal to "", it is a question
         if( (keccak256(abi.encodePacked((_link))) == keccak256(abi.encodePacked(("")))) ){
@@ -93,17 +73,23 @@ contract Data {
                 assert(hasAnswerLink(_url));
             }else{
                 answerLink.push(_url);
+                // collect answer per question
+                getOneAnswer[_url].push(_data);
+                // collect all answer of each question
                 addAnswer(_link,_data);
             }
-            
         }
         setValue(_url);
     }
-
+    ///@param _url questionlink of answer
+    ///@param _data data of question
+    // add answer of each question
     function addAnswer(string memory _url, string memory _data) public {
         getAnswer[_url].push(_data);
     }
-
+    ///@param _url question url
+    ///@param _id UUID from code snippet
+    // update question UUID, which generate from code snippet
     function updateUUIDToQuestion(string memory _url, uint _id) private {
         // check url in array and add uuid of code snippet in struct of stackoverflowInfo
         if(contains(_url)){
@@ -115,12 +101,17 @@ contract Data {
             getQuestion[_url].uuidFromSnippet = 0;
         }
     }
-
+    ///@param _url question link of answer
+    ///@param _id UUID from code snippet
+    // update answer UUID, which generate from code snippet
     function updateUUIDToAnswer(string memory _url, uint _id) private {
         getUUIDFromAnswer[_url].push(_id);
     }
 
-    // duplicate!!!!!!!!!!
+    ///@param _url url from csv file
+    ///@param _link question url (if url is an answer link)
+    ///@param _data data of code snippet
+    // create code snippet and generate UUID for question and answer
     function createNewCodeSnippet(string memory _url, string memory _link, string memory _data) public {
         // create new code snippet and generate uuid
         // push new code snippet into map getCodeSnippet that link with _url
@@ -139,89 +130,139 @@ contract Data {
         // emit newCodeSnippetAdded(newCode.url, newCode.date, newCode.language, newCode.projectName,
         // newCode.companyName, newCode.projectPath, newCode.projectAuthor);
     }
-
+    
+    ///@param _url code snippet url
+    // get index of code snippet
     function getIndexOfSnippet(string memory _url) public view returns (uint) {
         return (getSnippet[_url].length);
     }
 
-    function getIndexOfAnswer(string memory _url) public view returns (uint) {
+    ///@param _url question link of answer
+    // get index of answer
+    function getIndexOfAnswerFromQuestionLink(string memory _url) public view returns (uint) {
         return (getAnswer[_url].length);
     }
 
+    ///@param _url question url
+    // get detail of each question
     function getDetailQuestion(string memory _url) public view returns (uint, string memory, bool){
         return (getQuestion[_url].uuidFromSnippet,
                 getQuestion[_url].data,
                 getQuestion[_url].isValue);
     }
-
-    function getDetailAnswer(string memory _url) public view returns (string[] memory){
+    
+    ///@param _url answer url
+    // get answer from each answer link
+    function getAnswerFromAnswerLink(string memory _url) public view returns (string[] memory){
+        return (getOneAnswer[_url]);
+    }
+    
+    ///@param _url question url
+    // get all answer from each question link
+    function getAnswerFromQuestionLink(string memory _url) public view returns (string[] memory){
         return (getAnswer[_url]);
     }
 
+    ///@param _url question url of answer or answer url
+    // can get answer per each question and all answer
+    function getDetailAnswer(string memory _url) public view returns (string[] memory){
+        if(hasAnswerLink(_url)){
+            return getAnswerFromAnswerLink(_url);
+        }else{
+            return getAnswerFromQuestionLink(_url);
+        }
+    }
+
+    ///@param _url question url
+    // get question and answer of this url
     function getQAndA(string memory _url) public view returns (uint, string memory, string[] memory){
         return (getQuestion[_url].uuidFromSnippet,
                 getQuestion[_url].data,
-                getAnswer[_url]);
+                getAnswerFromQuestionLink(_url));
     }
-
+    ///@param _url code snippet url
+    ///@param _index index of code snippet
+    // get detail of code snippet
     function getDetailSnippet(string memory _url, uint _index) public view returns (string memory, uint){
         // require((_index > getSnippet[_url].length) || (_index < 0),"index out of bound");
         return (getSnippet[_url][_index].data,
                 getSnippet[_url][_index].uuid);
     }
 
+    ///@param s code snippet data that will be changed to UUID
+    // generate UUID
     function getUUID(string memory s) public pure returns (uint){
         bytes memory data = stringToBytes(s);
        return uint(keccak256(abi.encodePacked(data)));
     }
 
+    ///@param _url question url
+    // get question UUID
     function getUUIDQuestion(string memory _url) public view returns (uint){
         return getUUIDFromQuestion[_url];
     }
 
+    ///@param _url answer url
+    // get answer UUID
     function getUUIDAnswer(string memory _url, uint _index) public view returns (uint){
         // require((_index > getUUIDFromAnswer[_url].length) || (_index < 0),"index out of bound");
         return getUUIDFromAnswer[_url][_index];
     }
 
+    ///@param _url code snippet url
+    ///@param _index index of code snippet
+    // get code snippet UUID
     function getUUIDSnippet(string memory _url, uint _index) public view returns (uint){
         // require((_index > getSnippet[_url].length) || (_index < 0),"index out of bound");
         return getSnippet[_url][_index].uuid;
     }
 
-
+    ///@param s any string word, which you want to convert it to byte
+    // convert string to bytes
     function stringToBytes(string memory s) public pure returns (bytes memory){
         bytes memory b = bytes(s);
         return b;
     }
 
+    // convert block number to bytes
     function blockNumber() public view returns (bytes32) {
         return bytes32(block.number);
     }
 
+    ///@param data any bytes32, which you want to convert it to string
+    // convert bytes32 to string
     function bytes32ToString (bytes32 data) public pure returns (string memory) {
-    bytes memory bytesString = new bytes(32);
-    for(uint j = 0; j < 32; j++){
-        byte char = byte(bytes32(uint(data) * 2 ** (8 * j)));
-        if (char != 0) {
-            bytesString[j] = char;
+        bytes memory bytesString = new bytes(32);
+        for(uint j = 0; j < 32; j++){
+            byte char = byte(bytes32(uint(data) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[j] = char;
+            }
         }
+        return string(bytesString);
     }
-    return string(bytesString);
-}
 
+    ///@param _url question url
+    // set status of question
     function setValue(string memory _url) public{
         getQuestion[_url].isValue = true;
     }
 
+    ///@param _url question url
+    // check question contain in array
     function contains(string memory _url) public view returns (bool){
         return getQuestion[_url].isValue;
     }
 
+    ///@param a first string
+    ///@param b second string
+    // append first and second string together
     function appendString(string memory a, string memory b) public pure returns (string memory) {
         return string(abi.encodePacked(a, b));
     }
 
+    ///@param _url answer url
+    // check answer link contain in array
     function hasAnswerLink(string memory _url) public view returns (bool){
         bool check = false;
         for( uint i = 0; i < answerLink.length; i++){
--- test/testSimpleStorage.js
+++ test/testSimpleStorage.js
@@ -38,32 +38,32 @@ const Data = artifacts.require('Data');
         // })
 
         // YES!!
-        // it("create new snippet", async() => {
-        //    question = await contractInstance.createNewQuestion("www.q.com","","dataQ");
-        //    snippetQuestion = await contractInstance.createNewCodeSnippet("www.q.com","","snippetQ");
-        //    answer = await contractInstance.createNewQuestion("www.a.com","www.qq.com","data of answer");
-        //    snippetAnswer = await contractInstance.createNewCodeSnippet("www.a.com","www.qq.com","snippetA");
+        it("create new snippet", async() => {
+           question = await contractInstance.createNewQuestion("www.q.com","","dataQ");
+           snippetQuestion = await contractInstance.createNewCodeSnippet("www.q.com","","snippetQ");
+           answer = await contractInstance.createNewQuestion("www.a.com","www.qq.com","data of answer");
+           snippetAnswer = await contractInstance.createNewCodeSnippet("www.a.com","www.qq.com","snippetA");
 
-        //    getUUIDQuestion = await contractInstance.getUUIDQuestion("www.q.com");
-        //    getUUIDSnippetQ = await contractInstance.getUUIDSnippet("www.q.com",0);
-        //    getUUIDAnswer = await contractInstance.getUUIDAnswer("www.qq.com",0);
-        //    getUUIDSnippetA = await contractInstance.getUUIDSnippet("www.qq.com",0);
+           getUUIDQuestion = await contractInstance.getUUIDQuestion("www.q.com");
+           getUUIDSnippetQ = await contractInstance.getUUIDSnippet("www.q.com",0);
+           getUUIDAnswer = await contractInstance.getUUIDAnswer("www.qq.com",0);
+           getUUIDSnippetA = await contractInstance.getUUIDSnippet("www.qq.com",0);
 
-        //    console.log(getUUIDQuestion);
-        //    console.log(getUUIDSnippetQ);
-        //    console.log(getUUIDAnswer);
-        //    console.log(getUUIDSnippetA);
-        // })
+           console.log(getUUIDQuestion);
+           console.log(getUUIDSnippetQ);
+           console.log(getUUIDAnswer);
+           console.log(getUUIDSnippetA);
+        })
         
         // YES!!
-        // it("create a new question", async() => {
-        //     question = await contractInstance.createNewQuestion("www.q.com","","data");
-        //     getQuestion = await contractInstance.getDetailQuestion("www.q.com");
-        //     console.log(getQuestion);
-        //     await contractInstance.createNewCodeSnippet("www.q.com","snippet");
-        //     getQuestion2 = await contractInstance.getDetailQuestion("www.q.com");
-        //     console.log(getQuestion2);
-        // })
+        it("create a new question", async() => {
+            question = await contractInstance.createNewQuestion("www.q.com","","data");
+            getQuestion = await contractInstance.getDetailQuestion("www.q.com");
+            console.log(getQuestion);
+            await contractInstance.createNewCodeSnippet("www.q.com","snippet");
+            getQuestion2 = await contractInstance.getDetailQuestion("www.q.com");
+            console.log(getQuestion2);
+        })
         
         // YES!!   
         it("get answer", async() => {
@@ -74,51 +74,23 @@ const Data = artifacts.require('Data');
             // answer3 = await contractInstance.createNewQuestion("www.a.com","www.qqq.com","data of answer3");
 
             getAnswer = await contractInstance.getDetailAnswer("www.qq.com");
+            getOneAnswer = await contractInstance.getDetailAnswer("www.a.com");
             // getQAndA = await contractInstance.getQAndA("www.qq.com");
             console.log(getAnswer);
+            console.log(getOneAnswer);
+
         })
         
         // YES!!
-        // it("get Q and A", async() => {
-        //     question = await contractInstance.createNewQuestion("www.q.com","","data of question");
-        //     snippetQuestion = await contractInstance.createNewCodeSnippet("www.q.com","","snippetQ");
-        //     answer = await contractInstance.createNewQuestion("www.a1.com","www.q.com","data of answer");
-        //     answer2 = await contractInstance.createNewQuestion("www.a2.com","www.q.com","data of answer2");
-        //     getQAndA = await contractInstance.getQAndA("www.q.com");
-        //     console.log(getQAndA);
-        //     // console.log(getQAndA2);
-        // })
+        it("get Q and A", async() => {
+            question = await contractInstance.createNewQuestion("www.q.com","","data of question");
+            snippetQuestion = await contractInstance.createNewCodeSnippet("www.q.com","","snippetQ");
+            answer = await contractInstance.createNewQuestion("www.a1.com","www.q.com","data of answer");
+            answer2 = await contractInstance.createNewQuestion("www.a2.com","www.q.com","data of answer2");
+            getQAndA = await contractInstance.getQAndA("www.q.com");
+            console.log(getQAndA);
+            // console.log(getQAndA2);
+        })
 
 
     }); // end of contract
-
-// contract("SimpleStorage", async(accounts) => {
-//     let contractInstance;
-//     beforeEach(async() => {
-//         // runs before each test in this block
-//         contractInstance = await SimpleStorage.deployed();
-//         contractInstance.set(0);
-//       });
-    
-//     it("should set number", async() => {           
-//             await contractInstance.set(9);
-//             getAns = await contractInstance.get();
-
-//             // console.log("get = " + getAns.toString());
-
-//             assert.equal(9, getAns.toString());
-//         })
-
-    
-
-//     it("should add number", async() => {
-//             await contractInstance.set(5);
-//             addAns = await contractInstance.add(10);
-//             getAns = await contractInstance.get();
-            
-//             assert.equal(15, getAns.toString());
-//         })
-
-//     }); // end of contract
-
-   
